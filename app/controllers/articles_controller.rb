4a929e23f64d962ebcec7210bddd33d6b091426b
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2016-12-11 23:20:44 -0200

    Commit inicial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params[:article].permit(:title, :text))
-    # @article.id = 10
    
     @article.save
     redirect_to @article
-
-    # render plain: params[:article].inspect
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,2 @@
-class Article
-  include Cequel::Record
-
-  key :id, :timeuuid, auto: true
-  column :title, :text
-  column :text, :text
+class Article < ApplicationRecord
 end
