5a35452f1e8a39c2ef0574678dfcce9f934aad99
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2017-02-03 15:22:06 -0200

    Code coverage.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
+require "simplecov"
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-require "simplecov"
-SimpleCov.start
 
 class ActiveSupport::TestCase
   fixtures :all
