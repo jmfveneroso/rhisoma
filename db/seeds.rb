8b3afb79f8cba1cf91b0761813a55df5ddc1f5db
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2017-02-27 19:41:56 -0300

    Seeds for example graph.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(email: "user@example.com",
+admin = User.create!(email: "user@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -14,4 +14,27 @@ User.create!(email: "user@example.com",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+territory = Territory.create(name: "Example") do |t|
+  t.user = admin
+end
+
+json = ActiveSupport::JSON.decode(File.read('db/graph_seeds.json'))
+json['groups'].each do |a|
+  sg = StylingGroup.create(a) do |g|
+    g.user = admin
+  end
+  sg.save
+end
+
+json['nodes'].each do |a|
+  n = a['type'].constantize.new(a)
+  n.territory = territory
+  n.save
+end
+
+json['links'].each do |a|
+  e = Edge.create(a)
+  e.save
 end
